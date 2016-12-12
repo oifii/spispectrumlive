a5c1ad268ab5ec9cb77ec75305bd3fe56905e44b
Stephane Poirier <stephane.poirier@oifii.org>
2016-12-12 12:56:05 -0500

    first commit

---
+++ Release/spispectrumlive3.bat
@@ -0,0 +1,6 @@
+rem start spispectrumplay.exe testwav.wav 10 200 200 0 "audiospispectrumclass" "audiospispectrum title" "begin - copy.ahk" "end - copy.ahk" 1 100
+rem start spispectrumlive.exe "D:\oifii-org\httpdocs\ha-org\had\dj-oifii\house_wav\dj-oifii_heavyhouse(Shivers-Armin van Buuren)_007.wav" 3600 1 1 0 "audiospispectrumliveclass" "audiospispectrumlive title" "" "" 1 100 1920 1080 150
+rem start spispectrumlive.exe "Desktop Microphone (2- HD-5000 " 3600 1 1 0 "audiospispectrumliveclass" "audiospispectrumlive title" "" "" 1 100 1920 1080 150
+rem start spispectrumlive.exe "Desktop Microphone (2- HD-5000 " 3600 1 1 0 "audiospispectrumliveclass" "audiospispectrumlive title" "" "" 1 100 400 200 150
+rem start spispectrumlive.exe "Wave (E-MU E-DSP Audio Processo" 3600 1 1 0 "audiospispectrumliveclass" "audiospispectrumlive title" "" "" 1 100 1920 1080 150
+start spispectrumlive.exe "Wave (E-MU E-DSP Audio Processo" 3600 1001 1 0 "audiospispectrumliveclass" "audiospispectrumlive title" "" "" 1 100 400 200 150
